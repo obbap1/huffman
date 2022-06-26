28fc2802f38ddd909038925a8c6662e35ad19c95
Paschal Obba <paschalobba@gmail.com>
2022-06-26 21:23:59 +0100

    chore: fix tests

--- decoder/huffman_test.go
+++ decoder/huffman_test.go
@@ -3,15 +3,17 @@ package decoder
 import (
 	"testing"
 
-	"github.com/obbap1/huffman/common"
+	encoder "github.com/obbap1/huffman/encoder"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDecoderHuffman(t *testing.T) {
-	// useless test, i know
 	assert := assert.New(t)
 	input := "good day sir"
-	decoderHuffman := Init(input, &common.HuffManNode{})
-	originalStr, _ := decoderHuffman.Do()
-	assert.Equal(originalStr, "") // since the Huffman node is empty. The only way to test this is to hardcode the node
+	encoderHuffman := encoder.Init(input)
+	compressedStr, huffmanNode, encoderStats := encoderHuffman.Do()
+	decoderHuffman := Init(compressedStr, huffmanNode)
+	originalStr, decodedStats := decoderHuffman.Do()
+	assert.Equal(originalStr, input)
+	assert.Equal(encoderStats.OriginalByteSize, decodedStats.OriginalByteSize)
 }
